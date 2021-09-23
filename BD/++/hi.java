e9bbecda62f15213076f4d8c24704025bfd12716
Fidelponcio <yeray.rubner@yahoo.es>
2021-09-23 20:56:19 +0200

    .

---
+++ BD/++/hi.java
@@ -0,0 +1,16 @@
+{
+    import java.util.Scanner;
+
+    public static void main (String [] args)
+    {
+        public static Scanner teclado = new Scanner(System.in);
+    
+        int num= teclado.nextInt();
+        
+        if (num/2=0){
+            System.out.println("El número" +num+ " es par.");
+        }else{
+            System.out.println("El número" +num+ " es impar.")
+        }
+    }
+}
