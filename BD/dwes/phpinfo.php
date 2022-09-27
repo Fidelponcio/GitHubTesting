5ceaad2b5fc33b291b03a86c6e4ba0575f54ba21
Fidelponcio <yeray.rubner@yahoo.es>
2022-09-27 17:29:56 +0200

    .

---
+++ BD/dwes/phpinfo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <?php //De locos amigo ?>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo 'PHPINFO.php' ?></title>
+</head>
+<body>
+    <?php
+    //  phpinfo(); 
+        $A= 'Hi';
+        $a= 'Hello';
+        $name = 'Fidelponcio';
+
+        echo $A;
+     
+     
+     ?>
+     <h1>Welcome back <?php echo $name ?> </h1>
+</body>
+</html>
