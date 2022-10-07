3e99504e63cc19988a519ecc2cef6fcf1e29a508
Fidelponcio <yeray.rubner@yahoo.es>
2022-10-07 21:02:33 +0200

    .

--- LM/JAVASCRIPT/2ndYear/Tema03/clock/clock.js
+++ LM/JAVASCRIPT/2ndYear/Tema03/clock/clock.js
@@ -3,42 +3,50 @@ let minutes = document.querySelector(".min");
 let hours = document.querySelector(".hour");
 
 let intervalo = setInterval(function(){
-    /*let secs = Number(seconds.innerText);
+    //Cronómetro
+
+    let secs = Number(seconds.innerText);
     let mins = Number(minutes.innerText);
     let hs = Number(hours.innerText);
-        
+    
     secs++;
+    secs = checkTime(secs);
     seconds.innerText = secs;
     
-    if (secs.length == 1){
-        secs = '0' + secs;
-        
-    }
-    console.log(secs.length);
-    
     if (secs === 60){
-        secs = 0;
+        secs = '00';
         seconds.innerText = secs;
         mins++;
+        mins = checkTime(mins);
         minutes.innerText = mins;
-
+        
     }
     
-
     if (mins === 60){
-        mins = 0;
+        mins = '00';
         minutes.innerText = mins;
         hs++;
+        hs = checkTime(hs);
         hours.innerText = hs;
     }
 
     if (hs === 24){
-        hs = 0;
+        hs = '00';
         hs.innerText = hs;
 
-    }*/
+    }
 
-    let date = new Date();
+    function checkTime(i){
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+
+
+    //Reloj
+
+    /*let date = new Date();
     var currSec = date.getSeconds();
     var currMins = date.getMinutes();
     var currHs = date.getHours();
@@ -46,8 +54,6 @@ let intervalo = setInterval(function(){
     currSec = checkTime(currSec);
     currMins = checkTime(currMins);
     currHs = checkTime(currHs);
-
-    
     
     seconds.innerText = currSec;
     minutes.innerText = currMins;
@@ -58,6 +64,6 @@ let intervalo = setInterval(function(){
             i = "0" + i;
         }
         return i;
-    }
+    }*/
 
 },1000);
