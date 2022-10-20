db578fb35e92d132d52c896d44633ae77d44d183
Fidelponcio <yeray.rubner@yahoo.es>
2022-10-20 19:25:27 +0200

    .

--- LM/JAVASCRIPT/2ndYear/Tema05_ObjetosPredefinidos/Strings/Ej01.js
+++ LM/JAVASCRIPT/2ndYear/Tema05_ObjetosPredefinidos/Strings/Ej01.js
@@ -3,3 +3,9 @@ A partir de las siguientes cadenas de texto javier,jose,pedro,luis realizar las 
 generar las cadenas de Javier,Jose,Pedro,Luis.
 */
 
+let name1 = "javier", name2 = "jose", name3 = "pedro", name4 = "luis";
+let names = ["javier,jose,pedro,luis"];
+
+name1.replace(name1.charAt(0), name1.charAt(0).toUpperCase());
+
+console.log(name1);
